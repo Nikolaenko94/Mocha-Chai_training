24bd9af4fb4977c2e6585bd146ce4ecbae5d3c1f
Stanislav Nikolaenko <Stanislav_Nikolaenko@epam.com>
2021-06-25 15:23:50 +0300

    feat: add chai test with main and basic features for chai

--- test/specs/isNumberEven.spec.js
+++ test/specs/isNumberEven.spec.js
@@ -16,5 +16,14 @@ describe('isNumberEven positive scenarios', function() {
   it(`should return false if number is odd`, function() {
     //expect(validator.isNumberEven(5)).to.be.false;  
     expect(validator.isNumberEven(5)).to.be.equal(false);
-});
+  });
+  it(`chai test`, function() {
+    expect(5).to.equal(5);
+    expect(5).to.below(54);// <
+    expect(5).to.above(2);// >
+    expect({a:"aple",b:43}).to.have.property("a");
+    expect({a:"apple",b:43}).to.have.property("a","apple");
+    expect({a:"apple",b:43}).to.be.eql({a:"apple",b:43});
+    expect([1,2,'12']).to.have.lengthOf(3);
+  });
 });
