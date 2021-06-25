e7fcaddbdfbd51eb3ec75dd299359266c886f65a
Stanislav Nikolaenko <Stanislav_Nikolaenko@epam.com>
2021-06-25 15:00:12 +0300

    refactor: chai additions1

--- test/specs/isNumberEven.spec.js
+++ test/specs/isNumberEven.spec.js
@@ -10,6 +10,11 @@ describe('isNumberEven positive scenarios', function() {
     validator = null;
   });
   it(`should return true if number is even`, function() {
+      //expect(validator.isNumberEven(4)).to.be.true;
       expect(validator.isNumberEven(4)).to.be.equal(true);
   });
+  it(`should return false if number is odd`, function() {
+    //expect(validator.isNumberEven(5)).to.be.false;  
+    expect(validator.isNumberEven(5)).to.be.equal(false);
+});
 });
